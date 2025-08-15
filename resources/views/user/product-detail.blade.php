d5eb7a834dd3262c4b4f27be790e0c873b10bdcc
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2025-08-16 00:50:11 +0530

    solving image issues

--- resources/views/user/product-detail.blade.php
+++ resources/views/user/product-detail.blade.php
@@ -667,12 +667,7 @@
                                         <div class="card custom-card wine-card">
                                             <!-- Image -->
                                             <div class="image-wrapper" style="position: relative;">
-                                                @php
-                                                    $primaryImage =
-                                                        $product->images->where('is_primary', true)->first() ??
-                                                        $product->images->first();
-                                                @endphp
-                                                <img src="{{ $primaryImage ? asset('storage/products/' . $primaryImage->image_path) : asset('images/default.jpg') }}"
+                                                <img src="{{ asset('storage/' . $product->image1) }}"
                                                     class="card-img-top rounded-0" alt="{{ $product->wine_name }}">
 
                                                 @if ($product->is_featured == 1)
