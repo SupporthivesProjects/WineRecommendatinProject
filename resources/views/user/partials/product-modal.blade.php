979a43b640967901b5c6cddc9ff4ddeb299fa911
Jitendra <nerdcode@Jitendras-MacBook-Air.local>
2026-06-12 12:18:33 +0530

    final modal change

--- resources/views/user/partials/product-modal.blade.php
+++ resources/views/user/partials/product-modal.blade.php
@@ -27,16 +27,57 @@
             </div>
 
             {{-- RIGHT SIDE CONTENT --}}
+            @php
+                // Type icons + colors
+                $typeIcons = [
+                    'red' => '<i class="fas fa-wine-glass text-danger"></i>',
+                    'white' => '<i class="fas fa-wine-glass text-warning"></i>',
+                    'sparkling' => '<i class="fas fa-champagne-glasses text-info"></i>',
+                    'ros' => '<i class="fas fa-wine-glass text-pink"></i>',
+                    'dessert' => '<i class="fas fa-ice-cream text-warning"></i>',
+                    'bordeaux' => '<i class="fas fa-wine-bottle text-purple"></i>',
+                ];
+
+                // Method icons + colors
+                $methodIcons = [
+                    'still' => '<i class="fas fa-tint text-primary"></i>',
+                    'semi sparkling' => '<i class="fas fa-bubbles text-info"></i>', // may need a replacement icon if bubbles not available
+                    'sparkling' => '<i class="fas fa-champagne-glasses text-warning"></i>',
+                    'fortified' => '<i class="fas fa-wine-bottle text-danger"></i>',
+                ];
+
+                // General default icon
+                $defaultIcon = '<i class="fas fa-tag text-secondary"></i>';
+
+                // Extra field icons
+                $extraIcons = [
+                    'sp_mentions' => '<i class="fas fa-comment-alt text-muted"></i>',
+                    'vintage_year' => '<i class="fas fa-calendar-alt text-secondary"></i>',
+                    'alcohol_vol' => '<i class="fas fa-percentage text-danger"></i>',
+                    'nature' => '<i class="fas fa-leaf text-success"></i>',
+                    'body' => '<i class="fas fa-balance-scale text-info"></i>',
+                    'time_spent_aging' => '<i class="fas fa-hourglass-half text-warning"></i>',
+                    'closure_type' => '<i class="fas fa-wine-bottle text-secondary"></i>',
+                    'serving_temperature' =>
+                        '<i class="fas fa-thermometer-half text-primary"></i>',
+                    'ageging_potential' => '<i class="fas fa-clock text-secondary"></i>',
+                    'cheese_pairing' => '<i class="fas fa-cheese text-warning"></i>',
+                    'importer_info' => '<i class="fas fa-truck text-secondary"></i>',
+                ];
+
+                $type = strtolower($product->type ?? 'n/a');
+                $method = strtolower($product->method ?? 'n/a');
+            @endphp
             <div class="col-lg-7">
                 <h2 class="fw-bold mb-2">
                     {{ $product->wine_name }}
                 </h2>
                 <p class="text-warning fw-semibold mb-2">
-                    {{ ucfirst($product->type) }}
+                    {!! $typeIcons[$type] ?? $defaultIcon !!} {{ ucfirst($product->type) ?? 'N/A' }}
                 </p>
                 <p class="mb-1">
                     <strong>Method:</strong>
-                    {{ $product->method ?? 'N/A' }}
+                    {!! $methodIcons[$method] ?? $defaultIcon !!} {{ ucfirst($product->method) ?? 'N/A' }}
                 </p>
                 <p class="mb-1">
                     <strong>Grape Variety:</strong>
@@ -118,55 +159,58 @@
                         <tbody>
                             <tr>
                                 <th width="35%">Type</th>
-                                <td>{{ ucfirst($product->type) ?? 'N/A' }}</td>
+                                <td>{!! $typeIcons[$type] ?? $defaultIcon !!} {{ ucfirst($product->type) ?? 'N/A' }}</td>
                             </tr>
                             <tr>
                                 <th>Method</th>
-                                <td>{{ ucfirst($product->method) ?? 'N/A' }}</td>
+                                <td>{!! $methodIcons[$method] ?? $defaultIcon !!} {{ ucfirst($product->method) ?? 'N/A' }}</td>
                             </tr>
                             <tr>
                                 <th>SP Mentions</th>
-                                <td>{{ $product->sp_mentions ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['sp_mentions'] ?? $defaultIcon !!} {{ $product->sp_mentions ?? 'N/A' }}</td>
                             </tr>
                             <tr>
                                 <th>Vintage</th>
-                                <td>{{ $product->vintage_year ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['vintage_year'] ?? $defaultIcon !!} {{ $product->vintage_year ?? 'N/A' }}</td>
                             </tr>
                             <tr>
                                 <th>Alcohol Volume</th>
-                                <td>{{ $product->alcohol_vol ?? 'N/A' }}%</td>
+                                <td>{!! $extraIcons['alcohol_vol'] ?? $defaultIcon !!} {{ $product->alcohol_vol }}%</td>
                             </tr>
                             <tr>
                                 <th>Nature</th>
-                                <td>{{ $product->nature ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['nature'] ?? $defaultIcon !!} {{ $product->nature }}</td>
                             </tr>
                             <tr>
                                 <th>Body</th>
-                                <td>{{ $product->body ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['body'] ?? $defaultIcon !!} {{ $product->body }}</td>
                             </tr>
                             <tr>
                                 <th>Time spent in Aging</th>
-                                <td>{{ $product->time_spent_aging ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['time_spent_aging'] ?? $defaultIcon !!} {{ $product->time_spent_aging }}</td>
                             </tr>
                             <tr>
                                 <th>Closure Type</th>
-                                <td>{{ $product->closure_type ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['closure_type'] ?? $defaultIcon !!} {{ $product->closure_type }}</td>
                             </tr>
                             <tr>
                                 <th>Serving Temperature</th>
-                                <td>{{ $product->serving_temperature ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['serving_temperature'] ?? $defaultIcon !!} {{ $product->serving_temperature }}</td>
                             </tr>
                             <tr>
                                 <th>Aging Potential</th>
-                                <td>{{ $product->ageing_potential ?? 'N/A' }}</td>
+                                <td>{!! $extraIcons['ageging_potential'] ?? $defaultIcon !!} {{ $product->ageing_potential ?? 'N/A' }}
                             </tr>
                             <tr>
                                 <th>Cheese Pairing</th>
-                                <td>{{ $product->cheese_pairing ?? 'N/A' }}</td>
+                                <td style="white-space: normal; word-wrap: break-word;">
+                                                    {!! $extraIcons['cheese_pairing'] ?? $defaultIcon !!} {{ $product->cheese_pairing ?? 'N/A' }}
+                                </td>
                             </tr>
                             <tr>
                                 <th>Importer</th>
-                                <td>{{ $product->importer_info ?? 'N/A' }}</td>
+                                <td style="white-space: normal; word-wrap: break-word;">
+                                                    {!! $extraIcons['importer_info'] ?? $defaultIcon !!} {{ $product->importer_info ?? 'N/A' }}</td>
                             </tr>
                         </tbody>
                     </table>
